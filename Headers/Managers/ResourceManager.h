7218bb1685782833f384cc7834e87995b89bddc8
HermanUpmann <32879121+HermanUpmann@users.noreply.github.com>
2018-05-11 17:19:15 -0700

    *Added the resource manager, for now only can load a sprite by name and sprite sheet.

---
+++ Headers/Managers/ResourceManager.h
@@ -0,0 +1,23 @@
+#ifndef __RESOURCE_MANAGER_H__
+#define __RESOURCE_MANAGER_H__
+
+#include "cocos2d.h"
+#include "2d/CCSpriteFrameCache.h"
+
+using namespace cocos2d;
+using namespace std;
+
+class ResourceManager
+{
+public:
+	ResourceManager();
+	~ResourceManager();
+
+	static ResourceManager* getInstance();
+	void LoadSpriteSheet(string spriteSheetplist);
+	Sprite* LoadSprite(string spriteName);
+
+private:
+	static ResourceManager* mResourceManager;
+};
+#endif
