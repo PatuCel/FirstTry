ac10b5097caced9333034bc65270937a9b2dd1e3
HermanUpmann <32879121+HermanUpmann@users.noreply.github.com>
2018-05-15 09:23:16 -0700

    *added a json reader to the ResourceManager

--- Headers/Managers/ResourceManager.h
+++ Headers/Managers/ResourceManager.h
@@ -2,6 +2,9 @@
 
 #include "cocos2d.h"
 #include "2d/CCSpriteFrameCache.h"
+#include "external/json/document.h"
+#include <fstream>
+#include <string>
 
 using namespace cocos2d;
 using namespace std;
@@ -15,6 +18,7 @@ public:
 	static ResourceManager* getInstance();
 	void LoadSpriteSheet(string spriteSheetplist);
 	Sprite* LoadSprite(string spriteName);
+	rapidjson::Document* LoadJson(string jsonFileName);
 
 private:
 	static ResourceManager* mResourceManager;
--- Source/Managers/ResourceManager.cpp
+++ Source/Managers/ResourceManager.cpp
@@ -28,3 +28,22 @@ Sprite* ResourceManager::LoadSprite(string spriteName)
 {
 	return Sprite::createWithSpriteFrameName(spriteName);
 }
+
+rapidjson::Document* ResourceManager::LoadJson(string jsonFileName)
+{
+	rapidjson::Document* jsonDocument;
+
+	std::string fullpath = FileUtils::getInstance()->fullPathForFilename(jsonFileName);
+	ifstream file(fullpath.c_str());
+	string strTemp;
+	string strJson = "";
+	while (std::getline(file, strTemp))
+	{
+		strJson += strTemp;
+		strJson.push_back('\n');
+	}
+
+	bool error = jsonDocument->Parse<0>(strJson.c_str()).HasParseError();
+
+	return jsonDocument;
+}
