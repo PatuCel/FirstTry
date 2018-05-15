b6ec42d757a50096a81a0dfa6b9b2262cc240401
Antonio Luna <Khrozz.Developer@Gmail.com>
2018-05-15 15:13:57 -0700

    Adding Missing Header

---
+++ Headers/Managers/LevelManager.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "json/document-wrapper.h"
+
+#include "cocos2d.h"
+USING_NS_CC;
+
+class LevelManager
+{
+public:
+	static LevelManager* getInstance();
+
+	bool loadLevel(const std::string filePath);
+
+private:
+	static LevelManager* m_levelManager;
+	rapidjson::Document m_levelJson;
+
+	LevelManager();
+};
