77791e39dfbd1f17792237b4d8e7c323838011e7
Kikiphallin <efrainenriquej@gmail.com>
2018-05-14 19:52:13 -0700

    + Still working on catching the mouse event.

--- Headers/Managers/InputManager.h
+++ Headers/Managers/InputManager.h
@@ -8,7 +8,11 @@ public:
 	~InputManager();
 	cocos2d::Vec2 pos;
 
+
 private:
+	
 	const static int MAX_TOUCHES = 5;
+	void init();
+	cocos2d::Label * labelTouchLocations[MAX_TOUCHES];
 };
 
--- Source/Managers/InputManager.cpp
+++ Source/Managers/InputManager.cpp
@@ -1,9 +1,32 @@
-#include "InputManager.h"
+#include "Managers\InputManager.h"
+#include "cocos\base\CCEventDispatcher.h"
 
-
+USING_NS_CC;
 
 InputManager::InputManager()
 {
+}
+
+void InputManager::init()
+{
+
+	//Create the labels:
+	for (int i = 0; i < MAX_TOUCHES; ++i)
+	{
+		labelTouchLocations[i] = Label::createWithSystemFont("", "Arial", 42);
+		labelTouchLocations[i]->setVisible(false);
+		//this->addChild(labelTouchLocations[i]);
+	}
+
+	cocos2d::EventListenerMouse* mouseListener = EventListenerMouse::create();
+	mouseListener->onMouseDown = [] (cocos2d::Event* event) {
+		EventMouse* mouseEvent = dynamic_cast<EventMouse*>(event);
+		mouseEvent->getMouseButton();
+		std::stringstream labelmsg;
+		auto bounds = event->getCurrentTarget()->getBoundingBox();
+		if(bounds.containsPoint(tou
+	};
+
 }
 
 
