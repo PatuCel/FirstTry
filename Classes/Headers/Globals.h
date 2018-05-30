31db3d256aa759d56508639ba472b4636679c4a7
Salomon Sanchez <salomon.sanchez@gameloft.com>
2018-05-30 16:51:44 -0700

    reverting Globals.h

--- Classes/Headers/Globals.h
+++ Classes/Headers/Globals.h
@@ -1,7 +1,7 @@
 #pragma once
 
-//#define SCREEN_RESOLUTION_W 640
-//#define SCREEN_RESOLUTION_H 960
+#define SCREEN_RESOLUTION_W 640
+#define SCREEN_RESOLUTION_H 960
 
 #define  CC_SCENE_MENU_MAIN  "Logo.ccreator"
 #define CC_UI_HUD  "Hud.ccreator"
