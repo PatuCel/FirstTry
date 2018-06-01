5877fce10ce552a6615916e4231f7be28a446226
Salomon Sanchez <salomon.sanchez@gameloft.com>
2018-05-31 17:18:10 -0700

    fixing hud

--- Classes/Headers/Components/Hud.h
+++ Classes/Headers/Components/Hud.h
@@ -8,11 +8,14 @@ class Hud : public cocos2d::Layer
 private:
 	cocos2d::Label* scoreLabel;
 	cocos2d::ui::LoadingBar* hp;
+	cocos2d::Node* pauseMenu;
 	std::function<void(int id)> buttonPressedListener;
 public:
 	static Hud* createHud();
 	Hud();
 	bool init();
+	void ShowMenu();
+	void HideMenu();
 	void setScore(std::string score);
 	void setHP(float percent);
 	void addButtonPressedListener(std::function<void(int id)> buttonPressedListener);
