2edfe08063eea1cdad330f861fe10d4485898b16
Peter Leyva <peter.leyva.bazan@gmail.com>
2018-06-01 22:46:40 -0700

    Change the root folders to compile buildWin32.bat

--- tools/scripts/common_config.bat
+++ tools/scripts/common_config.bat
@@ -7,6 +7,6 @@ set ROOT_PATH=%~dp0\..\..\
 set EXE_NAME=FirstTry.exe
 set BM_EXE_NAME=bm_%EXE_NAME%
 
-set WIN_PATH=%ROOT_PATH%proj.win32\Debug.win32\
+set WIN_PATH=%ROOT_PATH%build\Debug.win32\
 set SOURCES_PATH=%ROOT_PATH%proj.win32\
 set SLN_PATH=%SOURCES_PATH%FirstTry.sln
